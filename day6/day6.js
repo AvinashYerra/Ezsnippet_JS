28bddf50d86b96a007bc951f04daddb9eae9fcf9
Yerra SRJ Avinash <101195675+AvinashYerra@users.noreply.github.com>
2023-09-11 19:29:45 +0530

    Add files via upload

---
+++ day6/day6.js
@@ -0,0 +1,15 @@
+// Variables are a storage containers
+
+let firstName = "Avinash";
+console.log(firstName);
+
+
+//illegal naming conventions:-
+// No reserved word like new
+// No starting with Number
+// No special characters should be involved
+
+// legal but not good practice:-
+// using snake case 
+// starting with capital letters
+// using random names
