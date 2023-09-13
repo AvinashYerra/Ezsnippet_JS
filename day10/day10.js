c4d6673783945de7595dff513b19f5b6c53b7034
Yerra SRJ Avinash <101195675+AvinashYerra@users.noreply.github.com>
2023-09-13 12:35:06 +0530

    Add files via upload

---
+++ day10/day10.js
@@ -0,0 +1,18 @@
+//1.Conditional Operators
+//Equality
+
+let a=10;
+let b=10;
+console.log(a==b);
+// Inequality  a!=b
+// Greater than Operator  a>b
+// Less than Operator   a<b
+// Greater than Or Equal to  a>=b
+// Less than or Equal to     a<=b
+
+
+// 2.Logical Operators
+
+// AND operator  && check if both are true
+// OR operator  || check if one is true 
+// NOT operator  ! general not operator 
