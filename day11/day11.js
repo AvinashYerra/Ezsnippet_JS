f961c78ec09bff5a43a1cbab66a690068cfe6a6b
Yerra SRJ Avinash <101195675+AvinashYerra@users.noreply.github.com>
2023-09-14 19:22:23 +0530

    Add files via upload

---
+++ day11/day11.js
@@ -0,0 +1,9 @@
+//strings and literals
+
+
+let firstName= 'Avinash'
+let age=25
+
+console.log(`My name is ${firstName} and I am ${age} years old`);//with using string literal that is using $ and flower brackets{}
+
+console.log('My name is '+ firstName + ' and i am '+ age + 'years old');//without using string literal only using plus(+)
---
+++ day12/day12.js
@@ -0,0 +1,16 @@
+//Decision Making (if-else)
+
+let myAge=25
+let herAge=17
+
+if(myAge>=21 && herAge>=18){
+    console.log(`allowed`);
+}else {
+    console.log(`not allowed`);
+}
+
+// if(condition){
+//     execute this
+// }else{
+//     execute this
+// }
