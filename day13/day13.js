3f50b09e597e14a7b61c585b75fd925b9ed1e45e
Yerra SRJ Avinash <101195675+AvinashYerra@users.noreply.github.com>
2023-09-15 19:22:50 +0530

    Add files via upload

---
+++ day13/day13.js
@@ -0,0 +1,11 @@
+let num1='10'
+let num2=20;
+console.log(Number(num1)+num2);  //type conversion : manually done by the user
+
+let test='Avii';
+
+console.log(test); //this is a string
+
+console.log(!test) //js automatically converts this into boolean this is called as type coercion
+
+console.log('3'-3); //result is zero because js automatically converts to number 
