ea29b6b4f899838bea8bc96953d52283a2fa4892
Yerra SRJ Avinash <101195675+AvinashYerra@users.noreply.github.com>
2023-09-24 23:19:32 +0530

    Add files via upload

---
+++ day14/day14.js
@@ -0,0 +1,14 @@
+//interview question add in this
+
+//== === 
+
+// console.log(12==12); //true;
+// console.log('12'==12); //false;
+// console.log(12===12); //true;
+// console.log('12'===12);  //false
+
+
+// // == only value  === compare the data type also
+// console.log([] === []); //this is always false because js compares object by reference not value
+
+
---
+++ day15/day15.js
@@ -0,0 +1,9 @@
+//falsy values
+//null unidentified 0 ''  nan(not a number)
+//similarly all other gives true like 1, 'a'
+
+if(NaN){
+    console.log("its true");
+}else {
+    console.log("its false");
+}
